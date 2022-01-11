2f8ad8223bca068ed5cb0460bd81dbdd267b1db8
tahhh <thadpongsermsook1@gmail.com>
2022-01-11 13:06:05 +0700

    Lab15_1 completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	for(int r = 1; r < N; r++){
+		for(int j = r; j > 0; j--){
+			if(d[j] > d[j-1]){
+				int temp = d[j];
+   				d[j] = d[j-1];
+   				d[j-1] = temp;
+			}
+		}
+		cout << "Pass " << r << ":";
+		for(int s = 0; s < N; s++){
+			cout << d[s] << " ";
+		}
+		cout << endl;
+	}
 }
 
 int main(){
